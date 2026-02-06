ad84cc4548ab5240b21ecfe4cba21a359bbe1ed5
Arleta Madej <arleta.madej@gmail.com>
2026-02-06 16:01:03 +0100

    items of shopping list are presented as table

--- KitchenAssistantFrontend/src/app/components/shopping-list-item/shopping-list-item.component.ts
+++ KitchenAssistantFrontend/src/app/components/shopping-list-item/shopping-list-item.component.ts
@@ -3,6 +3,7 @@ import { ShoppingListItem } from '../../common/shopping-list-item';
 import { ShoppingListItemService } from 'src/app/services/shopping-list-item.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ShoppingListService } from 'src/app/services/shopping-list.service';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-shopping-list-item',
@@ -10,17 +11,20 @@ import { ShoppingListService } from 'src/app/services/shopping-list.service';
   styleUrls: ['./shopping-list-item.component.css']
 })
 export class ShoppingListItemComponent implements OnInit {
-
+  showAddItemForm: boolean = false;
+  newItem: ShoppingListItem | undefined;
   items: ShoppingListItem[] = [];
   listName: string | null = '';
+  productNames: { [productId: number]: string } = {};
 
   constructor(private route: ActivatedRoute,
-    private Router: Router,
-    private shoppingListItemService: ShoppingListItemService) {}
+    private shoppingListItemService: ShoppingListItemService,
+    private productService: ProductService) {}
 
   ngOnInit(): void {
     this.loadListName();
     this.loadsItems();
+    this.loadAllProductNamesFromProductService();
   }
 
   loadListName(): void {
@@ -40,5 +44,46 @@ export class ShoppingListItemComponent implements OnInit {
       next: data => this.items = data,
       error: err => console.error('Failed to load shopping list items', err)
     });
+  }
+
+  loadProductNameById(productId: number): string {
+    if (this.productNames[productId]) {
+      return this.productNames[productId];
+    } else {
+      this.productService.getProduct(productId).subscribe({
+        next: product => {
+          this.productNames[productId] = product.name;
+        },
+        error: err => console.error(`Failed to load product name for ID ${productId}`, err)
+      });
+      return this.productNames[productId]??'Ładowanie...';
+    }
+  }
+
+  loadAllProductNamesFromProductService(): void {
+    this.productService.getProductList().subscribe({
+      next: products => {
+        products.forEach(product => {
+          this.productNames[product.id] = product.name;
+        }); 
+      },
+      error: err => console.error('Failed to load product names', err)
+    });
+  }
+
+  deleteItem(item: ShoppingListItem) {
+  throw new Error('Method not implemented.');
+  }
+  addItem() {
+  throw new Error('Method not implemented.');
+  }
+  increaseQuantity(arg0: any) {
+  throw new Error('Method not implemented.');
+  }
+  decreaseQuantity(arg0: any) {
+  throw new Error('Method not implemented.');
+  }
+  onCheckboxChange($event: Event) {
+  throw new Error('Method not implemented.');
   }
 }
